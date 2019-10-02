7763809581a2b514b2605d6142a320f4b51160fd
Dina Berry <41597107+diberry@users.noreply.github.com>
2019-10-02 15:10:17 -0700

    change region for name

--- documentation-samples/quickstarts/publish-knowledge-base/publish-kb.go
+++ documentation-samples/quickstarts/publish-knowledge-base/publish-kb.go
@@ -16,7 +16,7 @@ func main() {
 	var knowledge_base_id = "YOUR-KNOWLEDGE-BASE-ID";
 	var resource_key = "YOUR-RESOURCE-KEY";
 
-	var host = fmt.Sprintf("https://westus.api.cognitive.microsoft.com/qnamaker/v4.0/knowledgebases/%s", knowledge_base_id);
+	var host = fmt.Sprintf("https://YOUR-RESOURCE-NAME.api.cognitive.microsoft.com/qnamaker/v4.0/knowledgebases/%s", knowledge_base_id);
 	var content = bytes.NewBuffer([]byte(nil));
 
 	req, _ := http.NewRequest("POST", host, content)
